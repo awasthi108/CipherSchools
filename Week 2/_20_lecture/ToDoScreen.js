f049605298c3f2ff4216f38679b1a418a99066e8
Shushant Kumar Awasthi <sushantawasthi000@gmail.com>
2024-06-13 02:14:51 +0530

    Update ToDoScreen.js

--- Week 2/_20_lecture/ToDoScreen.js
+++ Week 2/_20_lecture/ToDoScreen.js
@@ -1,1 +1,25 @@
 
+import { useState } from "react";
+const ToDoscreem=()=>{
+    const [taskList,setTaskList]=useState([]);
+    return (
+               <div className="screen">
+                <h1 className="ui heading center">To Do List</h1>
+               <div>
+                <button onClick={(e)=>{
+                    setTaskList([
+                        ...taskList,
+                        {
+                            title:"Go to gym",
+                            description: "Going to gym is good for muscle growth.",
+                        },
+                    ]);
+                }} className="ui secondary button">Add Task</button>
+                {taskList.map((task) => (
+                    <Task />
+                ))}
+               </div>
+               </div>
+                );
+};
+export default ToDoscreem;
