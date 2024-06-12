583c9ff55ee0a602eee4eeccbd4c12040ceb5e8d
Shushant Kumar Awasthi <sushantawasthi000@gmail.com>
2024-06-12 13:35:23 +0530

    Create ToDoscreem.js

---
+++ Week 2/_18_lecture/ToDoscreem.js
@@ -0,0 +1,9 @@
+import { Component } from "react";
+class ToDoscreem extends Component{
+    render(){
+        return(
+       <h1 className="ui heading center">To Do List</h1>
+        )
+    }
+    }
+export default ToDoscreem;
